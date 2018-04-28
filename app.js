4c666da01e25bac47624679718061d406ebbe6df
Glen Pierce <glen@jostle.me>
2018-04-28 15:54:27 -0700

    simple route

--- app.js
+++ app.js
@@ -5,13 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,16 +30,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
-
-const bluzelle = require('bluzelle');
-const UUID = "someUUID";
-const someString = "someString";
-
-bluzelle.connect('ws://127.0.0.1:8100', UUID);
-
-bluzelle.create('key', someString).then(() => {
-    console.log('key has been created');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const bluzelle = require('bluzelle');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    const UUID = "someUUID";
+    const someString = "someString";
+
+    bluzelle.connect(process.env.SWARM_IP, UUID);
+
+    bluzelle.create('key', someString).then(() => {
+        console.log('key has been created');
+    });
 });
 
 module.exports = router;
