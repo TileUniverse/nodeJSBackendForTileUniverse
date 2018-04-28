5a73656c71f6f56b699f1ec7b9af14a7fba4d2fb
Glen Pierce <glen@jostle.me>
2018-04-28 16:11:32 -0700

    get route separate from post route.

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,32 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const bluzelle = require('bluzelle');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
     const UUID = "someUUID";
     const someString = "someString";
+    bluzelle.connect(process.env.SWARM_IP, UUID);
 
+    bluzelle.read('key').then((value) => {
+        console.log("value:" + value);
+        res.json(value);
+    },
+    error => {
+        console.log(error);
+        res.json(error);
+    });
+});
+
+router.post('/', function (req, res) {
+    const UUID = "someUUID";
+    const someString = "someString";
     bluzelle.connect(process.env.SWARM_IP, UUID);
 
     bluzelle.create('key', someString).then(() => {
         console.log('key has been created');
+    },
+    error => {
+        console.log(error);
     });
 });
 
